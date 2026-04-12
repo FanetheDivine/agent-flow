fbb74c24b80b8857cf73fc2a4d2dd9cf916fa9d6
FanetheDivine <1974269607@qq.com>
2026-04-12 18:54:15 +0800

    icon&panel
    点击活动栏icon 可以打开webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "agent-flow" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('agent-flow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from agent-flow!');
+	const openPanel = vscode.commands.registerCommand('agent-flow.openPanel', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'agentFlow',
+			'Agent Flow',
+			vscode.ViewColumn.Active,
+			{
+				enableScripts: true,
+				retainContextWhenHidden: true,
+				localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'dist')]
+			}
+		);
+		panel.webview.html = getWebviewContent(panel.webview, context.extensionUri);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(openPanel);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri): string {
+
+	return `
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agent Flow</title>
+</head>
+<body>
+    <div id="root">aaa</div>
+</body>
+</html>
+`;
+}
