c4b213c5b4ec8da89c980bfb429f494bbe9ff099
FanetheDivine <1974269607@qq.com>
2026-05-05 13:17:47 +0800

    优化tag展示

--- src/webview/components/FileRefChip.tsx
+++ src/webview/components/FileRefChip.tsx
@@ -88,7 +88,7 @@ export const FileRefChip: FC<Props> = ({ data, closable, onClose }) => {
     <Tag
       closable={closable}
       onClose={onClose}
-      style={{ margin: 0, cursor: canPreview ? 'pointer' : 'default', whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}
+      className='m-0 cursor-pointer break-all whitespace-pre-wrap'
       onClick={
         canPreview
           ? (e) => {
