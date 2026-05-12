2760fca7c9bb9890adc8409514800c171c1f3a6d
Fane <141594291+FanetheDivine@users.noreply.github.com>
2026-05-12 20:36:49 +0800

    Dev/0512 (#9)
    * agent 增加简介
    
    * 绘制mermaid
    
    * 优化默认flow
    
    * 取消对于AgentName的唯一性校验
    
    * 取消编辑弹窗对名称的唯一性校验
    
    ---------
    
    Co-authored-by: Fane <zhuoxin.yuan@bluefocus.com>

--- src/webview/components/AgentFlow/AgentNode/AgentEditModal/index.tsx
+++ src/webview/components/AgentFlow/AgentNode/AgentEditModal/index.tsx
@@ -63,6 +63,7 @@ export const AgentEditModal: FC<AgentEditModalProps> = (props) => {
     if (open && agent) {
       const newFormValue: Omit<Agent, 'id'> = {
         agent_name: agent.agent_name,
+        agent_desc: agent.agent_desc,
         model: agent.model,
         effort: agent.effort,
         agent_prompt: agent.agent_prompt,
@@ -112,21 +113,17 @@ export const AgentEditModal: FC<AgentEditModalProps> = (props) => {
         <FormItem
           name='agent_name'
           label='Agent 名称'
-          rules={[
-            { required: true, message: '请输入名称' },
-            () => ({
-              validator(_: any, value: string) {
-                const currentName = agent?.agent_name
-                const others = allAgents.filter((a) => a.agent_name !== currentName)
-                if (others.some((a) => a.agent_name === value)) {
-                  return Promise.reject(new Error('名称已存在'))
-                }
-                return Promise.resolve()
-              },
-            }),
-          ]}
+          rules={[{ required: true, message: '请输入名称' }]}
         >
           <Input />
+        </FormItem>
+
+        <FormItem
+          name='agent_desc'
+          label='Agent 简介'
+          tooltip='简要描述该 Agent 的职责与定位，会在系统提示词中作为任务上下文注入'
+        >
+          <Input placeholder='例如：负责代码评审，检查潜在 bug 与性能问题' />
         </FormItem>
 
         <div className='flex gap-4'>
